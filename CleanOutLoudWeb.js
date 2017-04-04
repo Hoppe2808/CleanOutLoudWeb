67522bd7bd8cec467883760dbad7074345698667
Ubuntu <ubuntu@ip-172-31-11-30.us-west-2.compute.internal>
2017-04-04 09:41:37 +0000

    changes

--- CleanOutLoudWeb.js
+++ CleanOutLoudWeb.js
@@ -199,6 +199,6 @@ app.post('/quiz/answer',function(req, res){
 })
 
 //Listen on serverport:
-app.listen(3000, '52.43.233.138' ,function(){
+app.listen(3000, 'ec2-52-43-233-138.us-west-2.compute.amazonaws.com' ,function(){
 	console.log("Server started on port 3000...")
 });
