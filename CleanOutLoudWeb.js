9dea54ace1da909f84d888cf845b8cd0d47fd845
hoppe2808 <seb.hop@hotmail.com>
2017-05-02 14:47:51 +0200

    Implementeret mange funktioner

--- CleanOutLoudWeb.js
+++ CleanOutLoudWeb.js
@@ -8,25 +8,15 @@ var app = express();
 var soap = require('soap');
 var url = 'http://ec2-52-43-233-138.us-west-2.compute.amazonaws.com:3769/col?wsdl';
 
-soap.createClient(url, function(err, client){
-	client.getWallMessages("", function(err, result){
-		var convertUser = JSON.stringify(result.user);
-		//var object = JSON.parse(convertUser);
-		console.log(convertUser);
-	});
-});
-
 //Get data from database here:
 var token = "";
 var error = "";
-var question = {
-	desc: "Hvor grim er Lukas?",
-	ans1: "Meget grim",
-	ans2: "Rigtig grim",
-	ans3: "Ultra grim",
-	ans4: "Enormt grim",
-	ans5: "Godylt grim"
-}
+var messages = [];
+var dates = [];
+var user = {};
+var users = [];
+var camps = [];
+var weight = [];
 
 //Setup for various frameworks:
 
@@ -72,23 +62,148 @@ app.get('/', function(req, res){
 		error: ''
 	});
 });
-
+app.get('/index', function(req, res){
+	res.render('index', {
+		title: 'Hovedmenu',
+		permission: user.userType
+	});
+});
+app.post('/wall', function(req, res){
+	error = "";
+	res.render('wall', {
+		title: "Væg",
+		messages: messages,
+		dates: dates,
+		error: error
+	});	
+});
+app.post('/writeMessage', function(req, res){
+	res.render('writeMessage', {
+		title: "Opret Ny Besked"
+	});
+});
+app.post('/writeMessage/submit', function(req, res){
+	var message = req.body.message;
+	var args = {
+		arg0: message,
+		arg1: token
+	}
+	soap.createClient(url, function(err, client){
+		client.addMessage(args, function(err, result){
+			error = err;
+			console.log(err);
+			if(err == null){
+				getMessages(res);
+				res.redirect('/index');	
+			}
+		});
+	});
+});
 app.post('/trash', function(req, res){
+	soap.createClient(url, function(err, client){
+		client.getCampsSortedInWeight("", function(err, result){
+			error = err;
+			if(error = null){
+				console.log("Camps recieved...");
+			}
+			var json = result;
+			for (var i = 0; i < json.return.length; i++) {
+				camps[i] = json.return[i].campName;
+				weight[i] = json.return[i].garbageWeight;
+			}
+			res.render('trash', {
+				title: "Skralde Menu",
+				camps: camps,
+				weight: weight,
+				permission: user.userType,
+				error: error
+			});
+		});
+	});
+});
+app.get('/trash', function(req, res){
 	res.render('trash', {
 		title: "Skralde Menu",
-		users: users
+		camps: camps,
+		weight: weight,
+		permission: user.userType,
+		error: error
+	});
+});
+app.post('/trash/add', function(req, res){
+	var camp = req.body.selectpicker;
+	var weightAdded = req.body.weight;
+	args = {
+		arg0: camp,
+		arg1: weightAdded,
+		arg2: token
+	}
+	soap.createClient(url, function(err, client){
+		client.setGarbage(args, function(err, result){
+			error = err;
+			if(error == null){
+				error = "Kilo ændret..";
+			}
+			console.log(error);
+				client.getCampsSortedInWeight("", function(err, result){
+					error = err;
+					if(error = null){
+						console.log("Camps recieved...");
+					}
+					var json = result;
+					for (var i = 0; i < json.return.length; i++) {
+						camps[i] = json.return[i].campName;
+						weight[i] = json.return[i].garbageWeight;
+					}
+					res.redirect("/trash");
+				});
+		});
 	});
 });
 app.get('/users', function(req, res){
 	res.render('users', {
 		title: "Bruger Menu",
+		users: users,
+		token: token,
 		error: error
 	});
 });
 app.post('/users', function(req, res){
 	res.render('users', {
 		title: "Bruger Menu",
+		users: users,
+		token: token,
 		error: ""
+	});
+});
+app.post('/deleteUser', function(req, res){
+	soap.createClient(url, function(err, client){
+		client.getCamps("", function(err, result){
+			res.redirect("/users");
+		});
+	});
+});
+app.post('/camp', function(req, res){
+	soap.createClient(url, function(err, client){
+		client.getCamps("", function(err, result){
+			error = err;
+			var json = result.return;
+			for (var i = 0; i < json.length; i++) {
+				camps[i] = json[i].campName;
+			}
+			res.render('camps', {
+				title: "Camp Menu",
+				camps: camps,
+				error: ""
+			});
+		});
+	});	
+});
+app.get('/camp', function(req, res){
+	res.render('camps', {
+		title: "Camp Menu",
+		camps: camps,
+		error: error
 	});
 });
 app.post('/events', function(req, res){
@@ -96,22 +211,40 @@ app.post('/events', function(req, res){
 		title: "Events Menu"
 	});
 });
-app.post('/quiz', function(req, res){
-	res.render('quiz', {
-		title: "Quiz Menu",
-		question: question
-	});
-});
-app.get('/quiz', function(req, res){
-	res.render('quiz', {
-		title: "Quiz Menu",
-		question: question,
-		error: error
-	});
-});
 app.post('/back', function(req, res){
 	res.render('index', {
-		title: "Hovedmenu"
+		title: "Hovedmenu",
+		permission: user.userType
+	});
+});
+app.post('/deleteCamp', function(req, res){
+	var name = req.body.selectpicker;
+	args = {
+		arg0: name,
+		arg1: token
+	}
+	soap.createClient(url, function(err, client){
+		client.deleteCamp(args, function(err, result){
+
+			res.redirect('/camp');
+		});
+	});
+});
+app.post('/createCamp', function(req, res){
+	var name = req.body.name;
+	var args = {
+		arg0: name,
+		arg1: token
+	}
+	soap.createClient(url, function(err, client){
+		client.addCamp(args, function(err, result){
+			error = err;
+			if(error == null){
+				console.log("New camp created...");
+				error = "Du har oprettet en camp";
+			}
+			res.redirect('/camp');
+		});
 	});
 });
 app.post('/login', function(req, res){
@@ -134,10 +267,17 @@ app.post('/login', function(req, res){
 			client.login(args, function(err, result){
 				error = err;
 				token = result.return;
+				args.arg1 = token;
 				if(err == null){
-					res.render('index', {
-						title: "Hovedmenu"
-					});	
+					getMessages(res);
+					client.getUser(args, function(err, result){
+						user = result.return;
+						res.render('index', {
+							title: "Hovedmenu",
+							permission: user.userType
+						});	
+						console.log("User %s logged in...", user.userName);
+					});
 					error = "";
 				}else{
 					res.render('login', {
@@ -161,12 +301,9 @@ app.post('/users/create', function(req, res){
 	var pass = req.body.password;
 	var repass = req.body.repassword;
 	var admin = req.body.ans;
-	console.log("HALLor" + admin);
-	if(admin == 1){
+	if(admin == 1 || admin == null){
 		admin = "user";
 	}else if(admin == 2){
-		admin = "manager";
-	}else{
 		admin = "admin";
 	}
 	if(uname === "" && pass === ""){
@@ -197,7 +334,11 @@ app.post('/users/create', function(req, res){
 			});
 		}
 	}
-	res.redirect('/users');
+	if(token == "null"){
+		res.redirect('/login');
+	}else{
+		res.redirect('/users');
+	}
 });
 app.post('/quiz/answer',function(req, res){
 	var answer = req.body.ans;
@@ -205,7 +346,34 @@ app.post('/quiz/answer',function(req, res){
 	console.log(answer);
 	res.redirect('/quiz');
 });
-
+function getMessages(res){
+	soap.createClient(url, function(err, client){
+		client.getWallMessages("", function(err, result){
+			error = err;
+			var json = result;
+			for (var i = 0; i < json.return.length; i++) {
+				messages[i] = json.return[i].text;
+				dates[i] = json.return[i].date;
+			}
+			console.log("Wall messages recieved...");
+		});
+	});
+}
+function updateCamps(){
+	soap.createClient(url, function(err, client){
+		client.getCampsSortedInWeight("", function(err, result){
+			error = err;
+			if(error = null){
+				console.log("Camps recieved...");
+			}
+			var json = result;
+			for (var i = 0; i < json.return.length; i++) {
+				camps[i] = json.return[i].campName;
+				weight[i] = json.return[i].garbageWeight;
+			}
+		});
+	});
+}
 //Listen on serverport:
 app.listen(3000, function(){
 	console.log("Server started on port 3000...");
